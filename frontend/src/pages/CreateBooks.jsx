1fc709117465422fc8b141c67a85f989f67a6d41
Nuan <107318376+huangnuan2022@users.noreply.github.com>
2024-07-01 01:51:27 -0400

    Add CreateBooks component with form and book creation functionality
    - Created `CreateBooks` component to handle adding new books
    - Implemented form inputs for title, author, and publish year
    - Integrated `axios` for sending POST request to the backend API to create a new book
    - Added loading state management with `Spinner` component for better user experience during form submission
    - Used `useNavigate` from `react-router-dom` for navigation after successful book creation
    - Included `BackButton` component for navigation back to the home page
    - Styled form and inputs for better user interface
    - Handled form submission and error handling in the `handelSaveBook` function

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
+import { Spinner } from "../components/Spinner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const CreateBooks = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handelSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .post("http://localhost:5555/books", data)
+      .then(() => {
+        setLoading(true);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error happened. Please check console");
+        console.log(error);
+      });
+  };
   return (
-    <div>CreateBooks</div>
-  )
-}
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4"> Create Book </h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+          <div className="my-4">
+            <label className="mr-4 text-xl text-gray-500">Title</label>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2 w-full"
+            />
+          </div>
+          <div className="my-4">
+            <label className="mr-4 text-xl text-gray-500">Author</label>
+            <input
+              type="text"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2 w-full"
+            />
+          </div>
+          <div className="my-4">
+            <label className="mr-4 text-xl text-gray-500">Publish Year</label>
+            <input
+              type="number"
+              value={publishYear}
+              onChange={(e) => setPublishYear(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2 w-full"
+            />
+          </div>
+          <button className="p-2 m-8 bg-sky-800" onClick={handelSaveBook}>
+            Save
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
