0b3bb46cae980c9630b88a3fc08321140ab20a87
Nuan <107318376+huangnuan2022@users.noreply.github.com>
2024-07-01 03:28:36 -0400

    Implemented DeleteBook component for deleting a book
    - Created `DeleteBook` component to handle the deletion of books
    - Implemented state management for loading using `useState`
    - Integrated `axios` for sending DELETE request to the backend API to delete a book
    - Added loading state management with `Spinner` component for better user experience during deletion
    - Used `useNavigate` from `react-router-dom` for navigation after successful deletion
    - Used `useParams` from `react-router-dom` to retrieve book ID from the URL
    - Included `BackButton` component for navigation back to the previous page
    - Styled confirmation dialog and button for better user interface
    - Handled deletion confirmation and error handling in the `handelDeleteBook` function

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
+import { Spinner } from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const handelDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+        alert("An error happened. Please check console");
+      });
+  };
+
   return (
-    <div>DeleteBook</div>
-  )
-}
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+          <h3 className="text-2xl">
+            Are You Sure You Want To Delete This Book?
+          </h3>
+          <button
+            className="p-4 bg-red-600 text-white m-8 w-full"
+            onClick={handelDeleteBook}
+          >
+            Yes, Delete it
+          </button>
+        </div>
+      )}
+      ;
+    </div>
+  );
+};
